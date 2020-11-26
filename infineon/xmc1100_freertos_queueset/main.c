c4570697e93f28098e6bf3c1904cf66d390ff1bd
pszyjaciel <pszyjaciel@interia.pl>
2020-11-26 21:19:58 +0100

    queueset working now with 4 tasks

--- infineon/xmc1100_freertos_queueset/main.c
+++ infineon/xmc1100_freertos_queueset/main.c
@@ -1,73 +1,98 @@
 // to bedzie dobry pszykuat na odbieranie danych przez UARTa:
 // https://microcontrollerslab.com/freertos-queue-set-arduino/
 
-//.. i huj jak zafsze..
-// wyglonda jakby sie kolejka zapelniala i huj i wjencej nie pszyjmuje.
-
 #include <DAVE.h>                 //Declarations from DAVE Code Generation (includes SFR declaration)
 #include "MyUtils.h"
 
 /* Define the lengths of the queues that will be added to the queue set. */
 #define QUEUE_LENGTH_1		1
 #define QUEUE_LENGTH_2		1
+#define QUEUE_LENGTH_3		1
+#define QUEUE_LENGTH_4		1
 
 /* Define the size of the item to be held by queue 1 and queue 2 respectively.
  The values used here are just for demonstration purposes. */
-#define ITEM_SIZE_QUEUE_1	sizeof( uint32_t )
+#define ITEM_SIZE_QUEUE_1	sizeof( uint8_t )
 #define ITEM_SIZE_QUEUE_2	sizeof( uint8_t )
-#define COMBINED_LENGTH ( QUEUE_LENGTH_1 + QUEUE_LENGTH_2 )
+#define ITEM_SIZE_QUEUE_3	sizeof( uint8_t )
+#define ITEM_SIZE_QUEUE_4	sizeof( uint8_t )
+#define COMBINED_LENGTH ( QUEUE_LENGTH_1 + QUEUE_LENGTH_2 + QUEUE_LENGTH_3 + QUEUE_LENGTH_4 )
 
 static QueueSetHandle_t xQueueSet;
-QueueHandle_t xQueue1, xQueue2;
-SemaphoreHandle_t xMutex;
+QueueHandle_t xQueue1, xQueue2, xQueue3, xQueue4;
+
+// function to convert decimal to hexadecimal
+void myPutHexByte(uint8_t n) {
+// char array to store hexadecimal number
+	char hexaDeciNum[100];
+
+// counter for hexadecimal number array
+	int i = 0;
+	while (n != 0) {
+		// temporary variable to store remainder
+		int temp = 0;
+
+		// storing remainder in temp variable.
+		temp = n % 16;
+
+		// check if temp < 10
+		if (temp < 10) {
+			hexaDeciNum[i] = temp + 48;
+			i++;
+		} else {
+			hexaDeciNum[i] = temp + 55;
+			i++;
+		}
+		n = n / 16;
+	}
+	// printing hexadecimal number array in reverse order
+	for (int j = i - 1; j >= 0; j--) {
+		SEGGER_RTT_PutChar(0, hexaDeciNum[j]);
+	}
+	SEGGER_RTT_PutChar(0, '\r');
+	SEGGER_RTT_PutChar(0, '\n');
+}
+
+void vProcessValueFromQueue4(uint8_t xReceivedFromQueue4) {
+	setByValue(4);
+//	SEGGER_RTT_WriteString(0, "vProcessValueFromQueue4()\r\n");	// 42-44 µsec
+	myPutHexByte(xReceivedFromQueue4);
+	setByValue(0);
+}
+
+void vProcessValueFromQueue3(uint8_t xReceivedFromQueue3) {
+	setByValue(3);
+//	SEGGER_RTT_WriteString(0, "vProcessValueFromQueue3()\r\n");	// 42-44 µsec
+	myPutHexByte(xReceivedFromQueue3);
+	setByValue(0);
+}
+
+void vProcessValueFromQueue2(uint8_t xReceivedFromQueue2) {
+	setByValue(2);
+//	SEGGER_RTT_WriteString(0, "vProcessValueFromQueue2()\r\n");	// 42-44 µsec
+	myPutHexByte(xReceivedFromQueue2);
+	setByValue(0);
+}
+
+void vProcessValueFromQueue1(uint8_t xReceivedFromQueue1) {
+	setByValue(1);
+//	SEGGER_RTT_WriteString(0, "vProcessValueFromQueue1()\r\n");
+	myPutHexByte(xReceivedFromQueue1);
+	setByValue(0);
+}
 
 // https://www.freertos.org/Embedded-RTOS-Queues.html
-static void myHighPrioTask(void *pvParameters) {
-	char myKey;
+static void myUARTTask(void *pvParameters) {
 	BaseType_t bt;
+	uint32_t xReceivedFromQueue1, xReceivedFromQueue2, xReceivedFromQueue3, xReceivedFromQueue4;
+
+	QueueSetMemberHandle_t xActivatedMember;
 
 	for (;;) {
 		//setByValue(3);
 		DIGITAL_IO_SetOutputHigh(&LED0);
 		DIGITAL_IO_SetOutputLow(&LED1);
 
-		if (!SEGGER_RTT_HasKey()) {
-//			myKey = 0;
-//			xQueueSend(xQueue1, &myKey, 0);
-//			xQueueSend(xQueue2, &myKey, 0);
-			//setByValue(4);
-			vTaskDelay(pdMS_TO_TICKS(50));
-			//setByValue(0);
-			continue;
-		}
-
-		myKey = SEGGER_RTT_GetKey();
-		if (myKey == 49) {
-			bt = xQueueSend(xQueue1, &myKey, 0);
-		} else if (myKey == 50) {
-			bt = xQueueSend(xQueue2, &myKey, 0);
-		}
-
-		vTaskDelay(pdMS_TO_TICKS(100));
-		setByValue(0);
-	}
-}
-
-void vProcessValueFromQueue1(uint32_t xReceivedFromQueue1) {
-	//SEGGER_RTT_printf(0, "vProcessValueFromQueue1(): %d\r\n", xReceivedFromQueue1);
-	SEGGER_RTT_WriteString(0, "vProcessValueFromQueue1()\r\n");
-	setByValue(1);
-}
-
-static void myTask1(void *pvParameters) {
-	QueueSetMemberHandle_t xActivatedMember;
-	uint32_t xReceivedFromQueue1;
-	BaseType_t bt;
-
-	while (1) {
-		//setByValue(2);
-		//xActivatedMember = xQueueSelectFromSet(xQueueSet, 200 / portTICK_PERIOD_MS);
-		//xActivatedMember = xQueueSelectFromSet(xQueueSet, 0);
 		xActivatedMember = xQueueSelectFromSet(xQueueSet, portMAX_DELAY);
 
 		if (xActivatedMember == xQueue1) {
@@ -75,41 +100,108 @@ static void myTask1(void *pvParameters) {
 			if (bt == pdTRUE) {
 				vProcessValueFromQueue1(xReceivedFromQueue1);
 			}
-		}
-
-		DIGITAL_IO_SetOutputLow(&LED0);
-		DIGITAL_IO_SetOutputHigh(&LED1);
-
-		//setByValue(0);
-		vTaskDelay(pdMS_TO_TICKS(200));
-	}
-}
-
-void vProcessValueFromQueue2(uint8_t xReceivedFromQueue2) {
-	//SEGGER_RTT_printf(0, "vProcessValueFromQueue2(): %d\r\n", xReceivedFromQueue2);	// wypierdala
-	SEGGER_RTT_WriteString(0, "vProcessValueFromQueue2()\r\n");
-	setByValue(2);
-}
-
-static void myTask2(void *pvParameters) {
-	QueueSetMemberHandle_t xActivatedMember;
-	uint8_t xReceivedFromQueue2;
-	BaseType_t bt;
-
-	while (1) {
-		//setByValue(1);
-		//xActivatedMember = xQueueSelectFromSet(xQueueSet, 200 / portTICK_PERIOD_MS);
-		//xActivatedMember = xQueueSelectFromSet(xQueueSet, 0);
-		xActivatedMember = xQueueSelectFromSet(xQueueSet, portMAX_DELAY);
-
-		if (xActivatedMember == xQueue2) {
+		} else if (xActivatedMember == xQueue2) {
 			bt = xQueueReceive(xActivatedMember, &xReceivedFromQueue2, 0);
 			if (bt == pdTRUE) {
 				vProcessValueFromQueue2(xReceivedFromQueue2);
 			}
+		} else if (xActivatedMember == xQueue3) {
+			bt = xQueueReceive(xActivatedMember, &xReceivedFromQueue3, 0);
+			if (bt == pdTRUE) {
+				vProcessValueFromQueue3(xReceivedFromQueue3);
+			}
+		} else if (xActivatedMember == xQueue4) {
+			bt = xQueueReceive(xActivatedMember, &xReceivedFromQueue4, 0);
+			if (bt == pdTRUE) {
+				vProcessValueFromQueue4(xReceivedFromQueue4);
+			}
+		} else {
+			// nic nie rup
+		}
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
+}
+
+static void myTask1(void *pvParameters) {
+
+	uint8_t myKey;
+	BaseType_t bt;
+
+	myKey = 0x80;
+	while (1) {
+		//setByValue(2);
+		DIGITAL_IO_SetOutputLow(&LED0);
+		DIGITAL_IO_SetOutputHigh(&LED1);
+
+		myKey++;
+		if (myKey < 0x80) {
+			myKey = 0x80;
+		}
+		bt = xQueueSend(xQueue1, &myKey, 0);
+		if (bt != pdTRUE) {
+			// kucha
+		}
+		//setByValue(0);
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
+}
+
+static void myTask2(void *pvParameters) {
+	uint8_t myKey;
+	BaseType_t bt;
+
+	myKey = 0x00;
+	while (1) {
+
+		myKey++;
+		if (myKey > 0x80) {
+			myKey = 0x00;
+		}
+		bt = xQueueSend(xQueue2, &myKey, 0);
+		if (bt != pdTRUE) {
+			// kucha
 		}
 
-		//setByValue(0);
+		vTaskDelay(pdMS_TO_TICKS(50));
+	}
+}
+
+static void myTask3(void *pvParameters) {
+	uint8_t myKey;
+	BaseType_t bt;
+
+	myKey = 0x00;
+	while (1) {
+
+		myKey++;
+		if (myKey > 0x80) {
+			myKey = 0x00;
+		}
+		bt = xQueueSend(xQueue3, &myKey, 0);
+		if (bt != pdTRUE) {
+			// kucha
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(150));
+	}
+}
+
+static void myTask4(void *pvParameters) {
+	uint8_t myKey;
+	BaseType_t bt;
+
+	myKey = 0x00;
+	while (1) {
+
+		myKey++;
+		if (myKey > 0x80) {
+			myKey = 0x00;
+		}
+		bt = xQueueSend(xQueue4, &myKey, 0);
+		if (bt != pdTRUE) {
+			// kucha
+		}
+
 		vTaskDelay(pdMS_TO_TICKS(250));
 	}
 }
@@ -137,46 +229,39 @@ int main(void) {
 	/* Create the queues and semaphores that will be contained in the set. */
 	xQueue1 = xQueueCreate( QUEUE_LENGTH_1, ITEM_SIZE_QUEUE_1);
 	xQueue2 = xQueueCreate( QUEUE_LENGTH_2, ITEM_SIZE_QUEUE_2);
+	xQueue3 = xQueueCreate(QUEUE_LENGTH_3, ITEM_SIZE_QUEUE_3);
+	xQueue4 = xQueueCreate(QUEUE_LENGTH_4, ITEM_SIZE_QUEUE_4);
 
 	/* Check everything was created. */
-	configASSERT(xQueueSet);
-	configASSERT(xQueue1);
-	configASSERT(xQueue2);
-
+	//configASSERT(xQueueSet);	// to makro to wielka hujnia
+	//configASSERT(xQueue1);
+	//configASSERT(xQueue2);
 	xQueueAddToSet(xQueue1, xQueueSet);
 	xQueueAddToSet(xQueue2, xQueueSet);
+	xQueueAddToSet(xQueue3, xQueueSet);
+	xQueueAddToSet(xQueue4, xQueueSet);
 
-	xMutex = xSemaphoreCreateMutex();
-	if (xMutex != NULL) {
-		bt = xTaskCreate(myHighPrioTask, "myHighPrioTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
-		if (bt != pdTRUE) {
-			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myHighPrioTask\r\n");
-		}
-		bt = xTaskCreate(myTask1, "WRITE_TASK_1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// RED
-		if (bt != pdTRUE) {
-			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask1\r\n");
-		}
-		bt = xTaskCreate(myTask2, "WRITE_TASK_2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// BLUE
-		if (bt != pdTRUE) {
-			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask2\r\n");
-		}
-//		bt = xTaskCreate(myTask3, "WRITE_TASK_3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// GREEN
-//		if (bt != pdTRUE) {
-//			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask3\r\n");
-//		}
-//		bt = xTaskCreate(myTask4, "WRITE_TASK_4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);// YELLOW
-//		if (bt != pdTRUE) {
-//			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask4\r\n");
-//		}
-//		bt = xTaskCreate(myTask5, "WRITE_TASK_5", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);// AQUAMARINE
-//		if (bt != pdTRUE) {
-//			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask5\r\n");
-//		}
-//		bt = xTaskCreate(myTask6, "WRITE_TASK_6", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// WHITE
-//		if (bt != pdTRUE) {
-//			SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask6\r\n");
-//		}
+	bt = xTaskCreate(myUARTTask, "myUARTTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+	if (bt != pdTRUE) {
+		SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myHighPrioTask\r\n");
 	}
+	bt = xTaskCreate(myTask1, "WRITE_TASK_1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// RED
+	if (bt != pdTRUE) {
+		SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask1\r\n");
+	}
+	bt = xTaskCreate(myTask2, "WRITE_TASK_2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// BLUE
+	if (bt != pdTRUE) {
+		SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask2\r\n");
+	}
+	bt = xTaskCreate(myTask3, "WRITE_TASK_3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// GREEN
+	if (bt != pdTRUE) {
+		SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask3\r\n");
+	}
+	bt = xTaskCreate(myTask4, "WRITE_TASK_4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);	// YELLOW
+	if (bt != pdTRUE) {
+		SEGGER_RTT_printf(0, "main(): bt != pdTRUE for myTask4\r\n");
+	}
+
 	/* Start scheduling. */
 	vTaskStartScheduler();
 
